c9618eb0ac07e8a30cef27409efe63af788f14be
Lucas Henrique <161850556+LucasHCF@users.noreply.github.com>
2024-04-01 11:45:46 -0300

    novos exercícios

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Você foi multado!')
+}
+console.log(`Dirija com sinto de segurnça!`)
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var pais = 'USA'
+console.log(`${pais}`)
+if (pais == 'Brasil'){
+    console.log('Brasileiro!')
+}else {
+    console.log('Estrangeiro!')
+}
